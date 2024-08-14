54249ead4e4e70a6734ccf4d12a8d9e9c8ee08e3
Ernesto José Riveiro <ernesto.ernzxr@gmail.com>
2024-08-14 02:25:38 -0300

    Completed exercises 1 to 4

---
+++ MiPrimerAplicacion/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace MiPrimerAplicacion
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.btnClick = new System.Windows.Forms.Button();
+            this.lbText = new System.Windows.Forms.Label();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.txtNuevo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnClick
+            // 
+            this.btnClick.BackColor = System.Drawing.Color.White;
+            this.btnClick.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnClick.FlatAppearance.BorderSize = 2;
+            this.btnClick.Font = new System.Drawing.Font("MS PGothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.btnClick.Image = ((System.Drawing.Image)(resources.GetObject("btnClick.Image")));
+            this.btnClick.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnClick.Location = new System.Drawing.Point(12, 73);
+            this.btnClick.Name = "btnClick";
+            this.btnClick.Size = new System.Drawing.Size(75, 23);
+            this.btnClick.TabIndex = 0;
+            this.btnClick.Text = "Click";
+            this.btnClick.UseVisualStyleBackColor = false;
+            this.btnClick.Click += new System.EventHandler(this.btnClick_Click);
+            // 
+            // lbText
+            // 
+            this.lbText.AutoSize = true;
+            this.lbText.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbText.Font = new System.Drawing.Font("Bebas Neue Pro Exp Eb", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbText.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.lbText.Location = new System.Drawing.Point(12, 9);
+            this.lbText.Name = "lbText";
+            this.lbText.Size = new System.Drawing.Size(300, 50);
+            this.lbText.TabIndex = 1;
+            this.lbText.Text = "App Development";
+            this.lbText.MouseLeave += new System.EventHandler(this.lbText_MouseLeave);
+            this.lbText.MouseMove += new System.Windows.Forms.MouseEventHandler(this.lbText_MouseMove);
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtDescription.Location = new System.Drawing.Point(156, 196);
+            this.txtDescription.MaxLength = 6;
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(101, 20);
+            this.txtDescription.TabIndex = 2;
+            this.txtDescription.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDescription_KeyPress);
+            // 
+            // txtNuevo
+            // 
+            this.txtNuevo.Location = new System.Drawing.Point(401, 233);
+            this.txtNuevo.Multiline = true;
+            this.txtNuevo.Name = "txtNuevo";
+            this.txtNuevo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtNuevo.Size = new System.Drawing.Size(184, 135);
+            this.txtNuevo.TabIndex = 3;
+            this.txtNuevo.Leave += new System.EventHandler(this.txtNuevo_Leave);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtNuevo);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.lbText);
+            this.Controls.Add(this.btnClick);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Introducción";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClick;
+        private System.Windows.Forms.Label lbText;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.TextBox txtNuevo;
+    }
+}
+
---
+++ MiPrimerAplicacion/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiPrimerAplicacion
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            //MessageBox.Show("Bienvenidos a mi primer aplicación en C#");
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            //MessageBox.Show("Chau Chau ...");
+        }
+
+
+        private void MainForm_MouseClick(object sender, MouseEventArgs e)
+        {
+            MouseEventArgs click = (MouseEventArgs)e;
+            if (click.Button == MouseButtons.Left)
+                MessageBox.Show("Presiono el botón Izquierdo", "Atención");
+            else if (click.Button == MouseButtons.Right)
+                MessageBox.Show("Presiono el Botón Derecho", "Atención");
+            else if (click.Button == MouseButtons.Middle)
+                MessageBox.Show("Presiono el botón del Medio", "Atención");
+        }
+
+        private void btnClick_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.WhiteSmoke;
+            if (txtDescription.Text == "")
+            {
+                txtDescription.BackColor = Color.Red;
+            }
+            else
+            {
+                txtDescription.BackColor = this.BackColor;
+                MessageBox.Show(Text = "Hola " + txtDescription.Text, "Saludo");
+            }
+        }
+
+        private void lbText_MouseMove(object sender, MouseEventArgs e)
+        {
+                lbText.BackColor = Color.WhiteSmoke;
+            lbText.Cursor = Cursors.Hand;
+        }
+
+        private void lbText_MouseLeave(object sender, EventArgs e)
+        {
+            lbText.BackColor = System.Drawing.SystemColors.Window;
+            lbText.Cursor = Cursors.Arrow;
+        }
+
+
+        private void txtDescription_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar < 48 || e.KeyChar > 59) && e.KeyChar != 8)
+                e.Handled = true;
+        }
+
+        private void txtNuevo_Leave(object sender, EventArgs e)
+        {
+            MessageBox.Show("Tiene " + txtNuevo.Text.Length + " Caracteres");
+        }
+    }
+}
