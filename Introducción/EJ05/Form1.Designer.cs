599154c10bfb2b473856636f37ac059fcb62095b
Ernesto José Riveiro <ernesto.ernzxr@gmail.com>
2024-08-14 15:48:07 -0300

    Completed exercise 5

---
+++ Introducción/EJ05/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace MiPrimerAplicacion
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblSurname = new System.Windows.Forms.Label();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnAccept = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            resources.ApplyResources(this.lblName, "lblName");
+            this.lblName.Name = "lblName";
+            // 
+            // lblSurname
+            // 
+            resources.ApplyResources(this.lblSurname, "lblSurname");
+            this.lblSurname.Name = "lblSurname";
+            // 
+            // lblAge
+            // 
+            resources.ApplyResources(this.lblAge, "lblAge");
+            this.lblAge.Name = "lblAge";
+            // 
+            // lblAddress
+            // 
+            resources.ApplyResources(this.lblAddress, "lblAddress");
+            this.lblAddress.Name = "lblAddress";
+            // 
+            // lblResult
+            // 
+            resources.ApplyResources(this.lblResult, "lblResult");
+            this.lblResult.Name = "lblResult";
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            resources.ApplyResources(this.txtSurname, "txtSurname");
+            this.txtSurname.Name = "txtSurname";
+            // 
+            // txtName
+            // 
+            this.txtName.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            resources.ApplyResources(this.txtName, "txtName");
+            this.txtName.Name = "txtName";
+            // 
+            // txtAge
+            // 
+            this.txtAge.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            resources.ApplyResources(this.txtAge, "txtAge");
+            this.txtAge.Name = "txtAge";
+            this.txtAge.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtAge_KeyPress);
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            resources.ApplyResources(this.txtAddress, "txtAddress");
+            this.txtAddress.Name = "txtAddress";
+            // 
+            // txtResult
+            // 
+            resources.ApplyResources(this.txtResult, "txtResult");
+            this.txtResult.Name = "txtResult";
+            // 
+            // btnAccept
+            // 
+            resources.ApplyResources(this.btnAccept, "btnAccept");
+            this.btnAccept.Name = "btnAccept";
+            this.btnAccept.UseVisualStyleBackColor = true;
+            this.btnAccept.Click += new System.EventHandler(this.btnAccept_Click);
+            // 
+            // btnCancel
+            // 
+            resources.ApplyResources(this.btnCancel, "btnCancel");
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // MainForm
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnAccept);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtSurname);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lblAddress);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.lblSurname);
+            this.Controls.Add(this.lblName);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblSurname;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnAccept;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
+
---
+++ Introducción/EJ05/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiPrimerAplicacion
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            //MessageBox.Show("Bienvenidos a mi primer aplicación en C#");
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            //MessageBox.Show("Chau Chau ...");
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txtAge_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar < 48 || e.KeyChar > 59) && e.KeyChar != 8)
+                e.Handled = true;
+        }
+
+        private void btnAccept_Click(object sender, EventArgs e)
+        {
+            string name = txtName.Text.Trim();
+            string surname = txtSurname.Text.Trim();
+            string age = txtAge.Text.Trim();
+            string address = txtAddress.Text.Trim();
+
+            if(name == "" || surname == "" || age == "" || address == "")
+            {
+                if (name == "")
+                {
+                    txtName.BackColor = Color.Red;
+                }
+                else
+                {
+                    txtName.BackColor = System.Drawing.SystemColors.Control;
+                }
+
+                if (surname == "")
+                {
+                    txtSurname.BackColor = Color.Red;
+                }
+                else
+                {
+                    txtSurname.BackColor = System.Drawing.SystemColors.Control;
+                }
+
+                if (age == "")
+                {
+                    txtAge.BackColor = Color.Red;
+                }
+                else
+                {
+                    txtAge.BackColor = System.Drawing.SystemColors.Control;
+                }
+
+                if (address == "")
+                {
+                    txtAddress.BackColor = Color.Red;
+                }
+                else
+                {
+                    txtAddress.BackColor = System.Drawing.SystemColors.Control;
+                }
+            }
+            else
+            {
+                txtName.BackColor = System.Drawing.SystemColors.Control;
+                txtSurname.BackColor = System.Drawing.SystemColors.Control;
+                txtAge.BackColor = System.Drawing.SystemColors.Control;
+                txtAddress.BackColor = System.Drawing.SystemColors.Control;
+
+                txtResult.Text = "Apellido y Nombre: " + surname + " " + name + Environment.NewLine +
+                                         "Edad: " + age + Environment.NewLine +
+                                         "Dirección: " + address;
+            }
+        }
+    }
+}
