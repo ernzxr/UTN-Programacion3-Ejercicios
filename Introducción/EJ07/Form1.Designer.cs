9abe241517037d12bb6270dedd439c2e7ea3e38e
Ernesto José Riveiro <ernesto.ernzxr@gmail.com>
2024-08-15 00:59:31 -0300

    Completed exercise 7

---
+++ Introducción/EJ07/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace EJ07
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBold = new System.Windows.Forms.Button();
+            this.btnUnderline = new System.Windows.Forms.Button();
+            this.btnItalic = new System.Windows.Forms.Button();
+            this.btnCenter = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.lblFontSize = new System.Windows.Forms.Label();
+            this.txtFontSize = new System.Windows.Forms.TextBox();
+            this.rtxtEditor = new System.Windows.Forms.RichTextBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnBold
+            // 
+            this.btnBold.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnBold.Location = new System.Drawing.Point(52, 31);
+            this.btnBold.Name = "btnBold";
+            this.btnBold.Size = new System.Drawing.Size(75, 23);
+            this.btnBold.TabIndex = 0;
+            this.btnBold.Text = "Negrita";
+            this.btnBold.UseVisualStyleBackColor = true;
+            this.btnBold.Click += new System.EventHandler(this.btnBold_Click);
+            // 
+            // btnUnderline
+            // 
+            this.btnUnderline.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnUnderline.Location = new System.Drawing.Point(133, 31);
+            this.btnUnderline.Name = "btnUnderline";
+            this.btnUnderline.Size = new System.Drawing.Size(75, 23);
+            this.btnUnderline.TabIndex = 1;
+            this.btnUnderline.Text = "Subrayado";
+            this.btnUnderline.UseVisualStyleBackColor = true;
+            this.btnUnderline.Click += new System.EventHandler(this.btnUnderline_Click);
+            // 
+            // btnItalic
+            // 
+            this.btnItalic.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnItalic.Location = new System.Drawing.Point(214, 31);
+            this.btnItalic.Name = "btnItalic";
+            this.btnItalic.Size = new System.Drawing.Size(75, 23);
+            this.btnItalic.TabIndex = 2;
+            this.btnItalic.Text = "Cursiva";
+            this.btnItalic.UseVisualStyleBackColor = true;
+            this.btnItalic.Click += new System.EventHandler(this.btnItalic_Click);
+            // 
+            // btnCenter
+            // 
+            this.btnCenter.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnCenter.Location = new System.Drawing.Point(295, 31);
+            this.btnCenter.Name = "btnCenter";
+            this.btnCenter.Size = new System.Drawing.Size(75, 23);
+            this.btnCenter.TabIndex = 3;
+            this.btnCenter.Text = "Centrado";
+            this.btnCenter.UseVisualStyleBackColor = true;
+            this.btnCenter.Click += new System.EventHandler(this.btnCenter_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnSave.Location = new System.Drawing.Point(133, 312);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Guardar";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnOpen.Location = new System.Drawing.Point(214, 312);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 5;
+            this.btnOpen.Text = "Abrir";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // lblFontSize
+            // 
+            this.lblFontSize.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblFontSize.AutoSize = true;
+            this.lblFontSize.Location = new System.Drawing.Point(130, 77);
+            this.lblFontSize.Name = "lblFontSize";
+            this.lblFontSize.Size = new System.Drawing.Size(82, 13);
+            this.lblFontSize.TabIndex = 6;
+            this.lblFontSize.Text = "Tamaño Fuente";
+            // 
+            // txtFontSize
+            // 
+            this.txtFontSize.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.txtFontSize.Location = new System.Drawing.Point(218, 74);
+            this.txtFontSize.MaxLength = 3;
+            this.txtFontSize.Name = "txtFontSize";
+            this.txtFontSize.Size = new System.Drawing.Size(51, 20);
+            this.txtFontSize.TabIndex = 7;
+            this.txtFontSize.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtFontSize_KeyPress);
+            this.txtFontSize.Validated += new System.EventHandler(this.txtFontSize_Validated);
+            // 
+            // rtxtEditor
+            // 
+            this.rtxtEditor.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtxtEditor.Location = new System.Drawing.Point(42, 109);
+            this.rtxtEditor.Name = "rtxtEditor";
+            this.rtxtEditor.Size = new System.Drawing.Size(338, 197);
+            this.rtxtEditor.TabIndex = 8;
+            this.rtxtEditor.Text = "";
+            this.rtxtEditor.LinkClicked += new System.Windows.Forms.LinkClickedEventHandler(this.rtxtEditor_LinkClicked);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnDelete.Location = new System.Drawing.Point(175, 340);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 9;
+            this.btnDelete.Text = "Borrar";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 375);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.rtxtEditor);
+            this.Controls.Add(this.txtFontSize);
+            this.Controls.Add(this.lblFontSize);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnCenter);
+            this.Controls.Add(this.btnItalic);
+            this.Controls.Add(this.btnUnderline);
+            this.Controls.Add(this.btnBold);
+            this.Name = "MainForm";
+            this.Text = "RichTextBox";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBold;
+        private System.Windows.Forms.Button btnUnderline;
+        private System.Windows.Forms.Button btnItalic;
+        private System.Windows.Forms.Button btnCenter;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Label lblFontSize;
+        private System.Windows.Forms.TextBox txtFontSize;
+        private System.Windows.Forms.RichTextBox rtxtEditor;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ Introducción/EJ07/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EJ07
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBold_Click(object sender, EventArgs e)
+        {
+            Font viejaFuente; //Declaro una variable Class Font para la Fuente Vieja.
+            Font nuevaFuente; //Declaro una variable Class Font para la Fuente Nueva.
+                              //Asigno a viejaFuente el tipo de fuente seleccionado.
+                              //Obtengo el tipo de Fuente atravez de la Propiedad SelectionFont
+                              //del Control RichTextBox.
+            viejaFuente = rtxtEditor.SelectionFont;
+            if (viejaFuente.Bold) //Pregunto si ya es Negrita.
+                                  //Si ya Tenia Negrita, establece nuevaFuente sin Negrita.
+                nuevaFuente = new Font(viejaFuente, viejaFuente.Style & ~FontStyle.Bold);
+            else
+                //Si no Tenia Negrita, establece nuevaFuente con Negrita.
+                nuevaFuente = new Font(viejaFuente, viejaFuente.Style | FontStyle.Bold);
+            //Establesco la nuevaFuente a la Selección.
+            rtxtEditor.SelectionFont = nuevaFuente;
+            //Realizo un foco para posicionarme en el Control.
+            rtxtEditor.Focus();
+        }
+
+        private void btnUnderline_Click(object sender, EventArgs e)
+        {
+            Font viejaFuente; //Declaro una variable Class Font para la Fuente Vieja.
+            Font nuevaFuente; //Declaro una variable Class Font para la Fuente Nueva.
+                              //Asigno a viejaFuente el tipo de fuente seleccionado.
+                              //Obtengo el tipo de Fuente atravez de la Propiedad SelectionFont
+                              //del Control RichTextBox.
+            viejaFuente = rtxtEditor.SelectionFont;
+            //Pregunto si ya es Subrayado.
+            //Si ya Tenia Subrayado, establece nuevaFuente sin Subrayado.
+            //Si no Tenia Subrayado, establece nuevaFuente con Subrayado.
+            nuevaFuente = new Font(viejaFuente, viejaFuente.Underline ? viejaFuente.Style & ~FontStyle.Underline : viejaFuente.Style | FontStyle.Underline);
+
+            //Establesco la nuevaFuente a la Selección.
+            rtxtEditor.SelectionFont = nuevaFuente;
+            //Realizo un foco para posicionarme en el Control.
+            rtxtEditor.Focus();
+        }
+
+        private void btnItalic_Click(object sender, EventArgs e)
+        {
+            Font viejaFuente; //Declaro una variable Class Font para la Fuente Vieja.
+            Font nuevaFuente; //Declaro una variable Class Font para la Fuente Nueva.
+                              //Asigno a viejaFuente el tipo de fuente seleccionado.
+                              //Obtengo el tipo de Fuente atravez de la Propiedad SelectionFont
+                              //del Control RichTextBox.
+            viejaFuente = rtxtEditor.SelectionFont;
+            if (viejaFuente.Italic) //Pregunto si ya es Cursiva.
+                                    //Si ya Tenia Cursiva, establece nuevaFuente sin Cursiva.
+                nuevaFuente = new Font(viejaFuente, viejaFuente.Style & ~FontStyle.Italic);
+            else
+                //Si no Tenia Cursiva, establece nuevaFuente con Cursiva.
+                nuevaFuente = new Font(viejaFuente, viejaFuente.Style | FontStyle.Italic);
+            //Establesco la nuevaFuente a la Selección.
+            rtxtEditor.SelectionFont = nuevaFuente;
+            //Realizo un foco para posicionarme en el Control.
+            rtxtEditor.Focus();
+        }
+
+        private void btnCenter_Click(object sender, EventArgs e)
+        {
+            //Atravez de la propiedad SelectionAlignment, obtiene la alineación
+            //del texto que se selecciono.
+            //Pregunto si es en centrado.
+            if (rtxtEditor.SelectionAlignment == HorizontalAlignment.Center)
+                //Si estaba Centrado lo alinea a la Izq.
+                rtxtEditor.SelectionAlignment = HorizontalAlignment.Left;
+            else
+                //Si no estaba centrado lo centra.
+                rtxtEditor.SelectionAlignment = HorizontalAlignment.Center;
+            rtxtEditor.Focus();
+        }
+
+        private void SetFontSize(string textSize)
+        {
+            //Declaro una Var. y le asigno el valor pasado, previa Conversion.
+            float nuevoTam = Convert.ToSingle(textSize);
+            //Declaro un Objeto FontFamily para el tipo de Fuente actual.
+            FontFamily actualFuenteFamily;
+            //Declaro un Objeto Font para la nueva Fuente.
+            Font nuevaFuente;
+            //De la selección actual del RichTextBox obtengo
+            //el tipo de familia de la fuente.
+            actualFuenteFamily = rtxtEditor.SelectionFont.FontFamily;
+            //New Font Instancio el objeto nuevaFuente segun
+            //tipo de FFuente y tamaño.
+            nuevaFuente = new Font(actualFuenteFamily, nuevoTam);
+            rtxtEditor.SelectionFont = nuevaFuente;
+        }
+        private void txtFontSize_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //Valido que si se presiono una tecla que no se un numero,
+            //Backspace o enter, se controle el evento.
+            if ((e.KeyChar < 48 || e.KeyChar > 57) && e.KeyChar != 8 && e.KeyChar != 13)
+                e.Handled = true;
+            //Pregunto si es Enter.
+            else if (e.KeyChar == 13)
+            {
+                //Si es Enter, pregunto si la cantidad de caracteres es mayor a 0.
+                if (txtFontSize.Text.Length > 0)
+                    //Llamo al metodo Aplicar Tamaño al Texto.
+                    SetFontSize(txtFontSize.Text);
+                //Si es Enter y no se ingresaron caracteres, controlo el evento.
+                e.Handled = true;
+                rtxtEditor.Focus();
+            }
+        }
+
+        private void txtFontSize_Validated(object sender, EventArgs e)
+        {
+            //El Evento Validated ocurre despues de la validación(Evento Validating).
+            //Llama al Metodo AplicarTamanioText y como parametro pasa,
+            //La propiedad Text del objeto sender(Objeto que disparo el evento)
+            //Del cual se hace un Cast del tipo TextBox.
+            //Es lo mismo que pasarle txtTamanio.text.
+            SetFontSize(((TextBox)sender).Text);
+            rtxtEditor.Focus();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                rtxtEditor.LoadFile("Test.rtf");
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                MessageBox.Show("No se pudo cargar el archivo");
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                rtxtEditor.SaveFile("Test.rtf");
+            }
+            catch (System.Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+        }
+
+        private void rtxtEditor_LinkClicked(object sender, LinkClickedEventArgs e)
+        {
+            //La calse Process brinda acceso a procesos locales y remotos.
+            //El metodo Start inicia un proceso y asocia el recurso a un nuevo componentes.
+            System.Diagnostics.Process.Start(e.LinkText);
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (System.IO.File.Exists("Test.rtf"))
+            {
+                try
+                {
+                    System.IO.File.Delete("Test.rtf");
+                    MessageBox.Show("Archivo eliminado exitosamente.");
+                }
+                catch (System.IO.IOException ioEx)
+                {
+                    MessageBox.Show("Error de E/S: " + ioEx.Message);
+                }
+                catch (System.UnauthorizedAccessException uaEx)
+                {
+                    MessageBox.Show("Acceso no autorizado: " + uaEx.Message);
+                }
+                catch (System.Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("El archivo no existe.");
+            }
+        }
+    }
+}
