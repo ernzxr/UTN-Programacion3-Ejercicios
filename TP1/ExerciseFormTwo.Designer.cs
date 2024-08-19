866be5bfe6f44b04cb9e51d27caa14ff1bb36f19
Ernesto José Riveiro <ernesto.ernzxr@gmail.com>
2024-08-19 02:37:49 -0300

    Completed ExerciseFormTwo of TP1

--- TP1/ExerciseFormTwo.Designer.cs
+++ TP1/ExerciseFormTwo.Designer.cs
@@ -28,12 +28,152 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblLastname = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnUnselect = new System.Windows.Forms.Button();
+            this.lbFullName = new System.Windows.Forms.ListBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblLastname);
+            this.groupBox1.Controls.Add(this.lblName);
+            this.groupBox1.Controls.Add(this.txtLastName);
+            this.groupBox1.Controls.Add(this.txtName);
+            this.groupBox1.Controls.Add(this.btnAdd);
+            this.groupBox1.Location = new System.Drawing.Point(19, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(255, 295);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ingreso de Datos";
+            // 
+            // lblLastname
+            // 
+            this.lblLastname.AutoSize = true;
+            this.lblLastname.Location = new System.Drawing.Point(107, 136);
+            this.lblLastname.Name = "lblLastname";
+            this.lblLastname.Size = new System.Drawing.Size(44, 13);
+            this.lblLastname.TabIndex = 4;
+            this.lblLastname.Text = "Apellido";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(107, 87);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(44, 13);
+            this.lblName.TabIndex = 3;
+            this.lblName.Text = "Nombre";
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(78, 152);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(100, 20);
+            this.txtLastName.TabIndex = 2;
+            this.txtLastName.TextChanged += new System.EventHandler(this.txtLastName_TextChanged);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(78, 103);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 1;
+            this.txtName.TextChanged += new System.EventHandler(this.txtName_TextChanged);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Enabled = false;
+            this.btnAdd.Location = new System.Drawing.Point(91, 188);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Agregar";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnUnselect);
+            this.groupBox2.Controls.Add(this.lbFullName);
+            this.groupBox2.Controls.Add(this.btnDelete);
+            this.groupBox2.Location = new System.Drawing.Point(306, 22);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(255, 295);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Elementos";
+            // 
+            // btnUnselect
+            // 
+            this.btnUnselect.Enabled = false;
+            this.btnUnselect.Location = new System.Drawing.Point(127, 257);
+            this.btnUnselect.Name = "btnUnselect";
+            this.btnUnselect.Size = new System.Drawing.Size(87, 23);
+            this.btnUnselect.TabIndex = 2;
+            this.btnUnselect.Text = "Deseleccionar";
+            this.btnUnselect.UseVisualStyleBackColor = true;
+            this.btnUnselect.Click += new System.EventHandler(this.btnUnselect_Click);
+            // 
+            // lbFullName
+            // 
+            this.lbFullName.FormattingEnabled = true;
+            this.lbFullName.Location = new System.Drawing.Point(22, 35);
+            this.lbFullName.Name = "lbFullName";
+            this.lbFullName.Size = new System.Drawing.Size(215, 212);
+            this.lbFullName.Sorted = true;
+            this.lbFullName.TabIndex = 3;
+            this.lbFullName.MouseClick += new System.Windows.Forms.MouseEventHandler(this.lbFullName_MouseClick);
+            this.lbFullName.SelectedValueChanged += new System.EventHandler(this.lbFullName_SelectedValueChanged);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Enabled = false;
+            this.btnDelete.Location = new System.Drawing.Point(46, 257);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 1;
+            this.btnDelete.Text = "Borrar";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // ExerciseFormTwo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "ExerciseFormTwo";
+            this.ClientSize = new System.Drawing.Size(582, 333);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.Name = "ExerciseFormTwo";
+            this.Text = "Nombre y Apellido";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblLastname;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox lbFullName;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUnselect;
     }
 }
--- TP1/ExerciseFormTwo.cs
+++ TP1/ExerciseFormTwo.cs
@@ -16,5 +16,97 @@ namespace TP1
         {
             InitializeComponent();
         }
+
+        private bool ValidateFullName(string fullName)
+        {
+            foreach (string _fullName in lbFullName.Items)
+            {
+                if (_fullName.ToUpper().Equals(fullName.ToUpper()))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void ValidateCompletedFullName()
+        {
+            if(txtName.Text.Trim().Length > 0 && txtLastName.Text.Trim().Length > 0)
+            {
+                btnAdd.Enabled = true;
+            }
+            else
+            {
+                btnAdd.Enabled = false;
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string _name = txtName.Text.Trim();
+            string _lastName = txtLastName.Text.Trim();
+            string _fullName = _name + " " + _lastName;
+            if(ValidateFullName(_fullName))
+            {
+                lbFullName.Items.Add(_fullName);
+                txtName.Text = "";
+                txtLastName.Text = "";
+                btnAdd.Enabled = false;
+            }
+            else
+            {
+                MessageBox.Show("La persona ya se encuentra registrada.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void txtName_TextChanged(object sender, EventArgs e)
+        {
+            ValidateCompletedFullName();
+        }
+
+        private void txtLastName_TextChanged(object sender, EventArgs e)
+        {
+            ValidateCompletedFullName();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if(lbFullName.SelectedItem != null)
+            {
+                lbFullName.Items.Remove(lbFullName.SelectedItem);
+                UpdateButtonsState();
+            }
+            else
+            {
+                MessageBox.Show("Seleccione un elemento de la lista.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void lbFullName_SelectedValueChanged(object sender, EventArgs e)
+        {
+            UpdateButtonsState();
+        }
+
+        private void UpdateButtonsState()
+        {
+            bool hasSelectedItem = lbFullName.SelectedItem != null;
+            btnDelete.Enabled = hasSelectedItem;
+            btnUnselect.Enabled = hasSelectedItem;
+        }
+
+        private void btnUnselect_Click(object sender, EventArgs e)
+        {
+            lbFullName.SelectedItem = null;
+            UpdateButtonsState();
+        }
+
+        private void lbFullName_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (lbFullName.IndexFromPoint(e.Location) == ListBox.NoMatches)
+            {
+                lbFullName.SelectedItem = null;
+                UpdateButtonsState();
+            }
+        }
     }
 }
